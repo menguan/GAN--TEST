3105e1c0a788119ab2e597dcc4060db4ebbcf230
menguan <qq1812755792@sina.com>
2018-08-10 09:47:42 +0800

    ll

--- main.py
+++ main.py
@@ -16,26 +16,23 @@ file = "D:/DATA/MNIST"
 
 mnist = input_data.read_data_sets(file, one_hot=True)
 
-## 权重变量
-def weight_variable(shape, name = ''):
-    initial = tf.truncated_normal(shape, stddev=0.01)
-    if name != '':
-        return tf.Variable(initial, name=name)
-    else:
-        return tf.Variable(initial)
-## 偏置 变量
-def bias_variable(shape):
-    initial = tf.constant(0.01, shape=shape)
-    return tf.Variable(initial)
+def weight_var(shape, name):
+    return tf.get_variable(name=name, shape=shape, initializer=tf.contrib.layers.xavier_initializer())
+
+
+def bias_var(shape, name):
+    return tf.get_variable(name=name, shape=shape, initializer=tf.constant_initializer(0))
+
+
 
 def upsample(input, size):
     return tf.image.resize_nearest_neighbor(input, size=(int(size), int(size)))
 
-def conv2d(input, in_features, out_features, kernel_size, st=1,with_bias=False):
-    W = weight_variable([ kernel_size, kernel_size, in_features, out_features ])
+def conv2d(input, in_features, out_features, kernel_size, name,st=1,with_bias=False):
+    W = weight_var([ kernel_size, kernel_size, in_features, out_features ],name=name)
     conv = tf.nn.conv2d(input, W, [ 1, st, st, 1 ], padding='SAME')
     if with_bias:
-        return tf.add(conv , bias_variable([ out_features ]))
+        return tf.add(conv , bias_var([ out_features ],name=name+'b'))
     return conv
 
 def max_pool(input, s, st=-1):
@@ -44,54 +41,64 @@ def max_pool(input, s, st=-1):
     return tf.nn.max_pool(input, [ 1, s, s, 1 ], [1, st, st, 1 ], 'SAME')
 
 def generator(input):
-    input=tf.reshape(input,shape=[-1,7,7,1])
+    
     with tf.variable_scope("generator",reuse=tf.AUTO_REUSE):
         
-        current=conv2d(input,1,4,3)
-        current=tf.nn.relu(current)
-        current=conv2d(current,4,4,3)
-        current=tf.nn.relu(current)
-        current=upsample(current,14)
-        current=conv2d(current,4,4,3)
-        current=tf.nn.relu(current)
-        current=conv2d(current,4,4,3)
-        current=tf.nn.relu(current)
-        current=upsample(current,28)
-        current=conv2d(current,4,4,3)
-        current=tf.nn.relu(current)
-        current=conv2d(current,4,4,3)
-        current=tf.nn.relu(current)
+#        current=conv2d(input,1,4,3)
+#        current=tf.nn.relu(current)
+#        current=conv2d(current,4,4,3)
+#        current=tf.nn.relu(current)
+#        current=upsample(current,14)
+#        current=conv2d(current,4,4,3)
+#        current=tf.nn.relu(current)
+#        current=conv2d(current,4,4,3)
+#        current=tf.nn.relu(current)
+#        current=upsample(current,28)
+#        current=conv2d(current,4,4,3)
+#        current=tf.nn.relu(current)
+#        current=conv2d(current,4,4,3)
+#        current=tf.nn.relu(current)
+        G_W1 = weight_var([100, 128], 'G_W1')
+        G_b1 = bias_var([128], 'G_B1')
+
+#        G_W2 = weight_var([128, 384], 'G_W2')
+#        G_b2 = bias_var([384], 'G_B2')
         
-        current=tf.reshape(current,shape=[-1,28*28*4])
-        G_W1 = weight_variable([28*28*4, 784])
-        G_b1 = bias_variable([784])
-        current = tf.matmul(current, G_W1) + G_b1
-        G_prob = tf.nn.sigmoid(current)
+        G_W3 = weight_var([128, 784], 'G_W3')
+        G_b3 = bias_var([784], 'G_B3')
+        
+        G_h2 = tf.nn.relu(tf.matmul(input, G_W1) + G_b1) 
+#        G_h2 = tf.nn.relu(tf.matmul(G_h1, G_W2) + G_b2) 
+        G_logit = tf.matmul(G_h2, G_W3) + G_b3 #batch*1
+        G_prob = tf.nn.sigmoid(G_logit)
         return G_prob
 
 def discriminator(input): 
     input=tf.reshape(input,shape=[-1,28,28,1])
-    with tf.variable_scope("discriminator",reuse=tf.AUTO_REUSE):
-        current=conv2d(input,1,2,3)
+    with tf.variable_scope("discriminator",reuse=tf.AUTO_REUSE):  
+        current=conv2d(input,1,2,3,name='D_C1')
         current=tf.nn.relu(current)
-        current=conv2d(current,2,2,3)
+        current=conv2d(current,2,4,3,name='D_C2')
         current=tf.nn.relu(current)
         current=max_pool(current,2) #14*14*2
-        current=conv2d(current,2,4,3)
-        current=tf.nn.relu(current)
-        current=conv2d(current,4,4,3)
-        current=tf.nn.relu(current)
-        current=max_pool(current,2) #7*7*4
-        current=conv2d(current,4,8,3)
-        current=tf.nn.relu(current)
-        current=conv2d(current,8,8,3)
-        current=tf.nn.relu(current)
-        current = tf.reshape(current, [ -1, 392 ])
+#        current=conv2d(current,2,4,3,name='D_C3')
+#        current=tf.nn.relu(current)
+#        current=conv2d(current,4,4,3,name='D_C4')
+#        current=tf.nn.relu(current)
+#        current=max_pool(current,2) #7*7*4
+#        current=conv2d(current,4,8,3)
+#        current=tf.nn.relu(current)
+#        current=conv2d(current,8,8,3)
+#        current=tf.nn.relu(current)
+        current = tf.reshape(current, [ -1, 784 ])
         
-        D_W1 = weight_variable([392, 128])
-        D_b1 = bias_variable([128])
-        D_W2 = weight_variable([128, 1])
-        D_b2 = bias_variable([1])
+        
+        D_W1 = weight_var([784, 128], 'D_W1')
+        D_b1 = bias_var([128], 'D_b1')
+
+        D_W2 = weight_var([128, 1], 'D_W2')
+        D_b2 = bias_var([1], 'D_b2')
+
         
         D_h1 = tf.nn.relu(tf.matmul(current, D_W1) + D_b1) #batch*64
         D_logit = tf.matmul(D_h1, D_W2) + D_b2 #batch*1
@@ -119,14 +126,14 @@ def plot(samples):
 def main():
 
     X = tf.placeholder(tf.float32, shape=[None, 784], name='X')
-    Z = tf.placeholder(tf.float32, shape=[None, 49], name='Z')
+    Z = tf.placeholder(tf.float32, shape=[None, 100], name='Z')
     lr = tf.placeholder("float", shape=[], name='learning_rate')
     
     smooth=0.01
-    batch_size = 128
-    Z_dim = 49
+    batch_size = 256
+    Z_dim = 100
     learning_rate_init=0.002
-    iter_time=1000
+    iter_time=10000
     show_init=randominit(16, Z_dim)
     
     
@@ -166,7 +173,7 @@ def main():
         elif it>=iter_time*0.8: learning_rate=learning_rate_init/16.0
         
         ####中间 画图
-        if it % 50 == 0:
+        if it % 100 == 0:
             samples = sess.run(G_sample, feed_dict={
                                Z: show_init})  
             fig = plot(samples)
@@ -185,7 +192,7 @@ def main():
                                   Z: randominit(batch_size, Z_dim)})
     #    writer = tf.summary.FileWriter("D://TensorBoard//test",sess.graph)
     
-        if it % 50 == 0:
+        if it % 100 == 0:
             print('Iter: {}'.format(it))
             print('D loss: {:.4}'.format(D_loss_curr))
             print('G_loss: {:.4}'.format(G_loss_curr))
